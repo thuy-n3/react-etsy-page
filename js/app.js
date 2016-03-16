997ce3bc95f49e7248c5f63c86b8f83d125690d4
Justin Richards <justinrichards@Justins-MacBook-Air.local>
2016-03-16 14:37:21 -0500

    cleaning up any staged and uncomitted changes

--- js/app.js
+++ js/app.js
@@ -26,7 +26,69 @@ import React, {Component} from 'react'
 function app() {
     // start app
     // new Router()
-    DOM.render(<p>test 2</p>, document.querySelector('.container'))
+    var AppView = React.createClass({
+
+    	_emptyUselessFunction: function() {
+
+    	},
+
+    	render: function() {
+    		return (
+    			<div className="pageContainer">			
+	    			<h1 className="headline">Iron Etsy</h1> 
+	    			<AboutResults/>
+	    			<ListingGrid/>
+	    		</div>
+    			)
+    	}
+    })
+
+   	var AboutResults = React.createClass({
+   		render: function() {
+   			return (
+   				<div className="about">
+	   				<p className="results">50,000 results</p>
+	   				<p className="showing">25 showing</p>
+   				</div>
+   				)
+   		}
+   	})
+
+   	var ListingGrid = React.createClass({
+
+   		_getListingsJSX: function() {
+   			var listings = []
+   			for (var i = 0; i < 4; i++) {
+   				var newListing = <Listing number={i} nickname="jesus the picture" />
+   				listings.push(newListing)
+   			}
+   			console.log(listings)
+   			return listings
+   		},
+
+   		render: function(){
+   			return (
+   				<div className="listingContainer">
+   					{/*js to be evaluated must go inside of squirrly brackets*/}
+   					{this._getListingsJSX()} 
+   				</div>
+   				)
+   		}
+   	})
+
+   	var Listing = React.createClass({
+   		render: function(){
+   			console.log(this)
+   			return (
+   				<div className="listing">
+   					<img src="http://lorempixel.com/200/200"/>
+   					<p className="descr">Picture {this.props.number}</p>
+   				</div>
+   				)
+   		}
+   	})
+
+    DOM.render(<AppView/>,document.querySelector('.container'))
 }
 
 app()
